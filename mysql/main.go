9828eb4dcb4f7b066a4712e8c179bb4198ff563b
Amr Farid <amr.farid222@gmail.com>
2020-09-11 19:18:12 +0200

    Adding crud

--- mysql/main.go
+++ mysql/main.go
@@ -4,13 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/sirupsen/logrus"
 	"os"
+	"time"
 )
 
 var (
 	db *sql.DB
 	err error
+	log *logrus.Logger
 )
+
+
 
 func init() {
 	dbUser := os.Getenv("DB_USER")
@@ -19,13 +24,97 @@ func init() {
 	fmt.Println(dsn)
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 }
 
 func main() {
   err = db.Ping()
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
+
+	{ // Create a new table
+		query := `
+            CREATE TABLE users (
+                id INT AUTO_INCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at DATETIME,
+                PRIMARY KEY (id)
+            );`
+
+		if _, err := db.Exec(query); err != nil {
+			log.Warn("table is already created")
+		}
+	}
+
+	{ // Insert a new user
+		username := "johndoe"
+		password := "secret"
+		createdAt := time.Now()
+
+		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := result.LastInsertId()
+		fmt.Println(id)
+	}
+
+	{ // Query a single user
+		var (
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		)
+
+		query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
+		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(id, username, password, createdAt)
+	}
+
+	{ // Query all users
+		type user struct {
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		}
+
+		rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []user
+		for rows.Next() {
+			var u user
+
+			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%#v", users)
+	}
+
+	{
+		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
